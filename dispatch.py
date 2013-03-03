6d56ee36144e8f890fd7bd919574fc22edfcfe03
Kenny Levinsen <kennylevinsen@gmail.com>
2013-03-03 02:34:52 +0100

    Wrap return data, lock various accesses

--- dispatch.py
+++ dispatch.py
@@ -4,36 +4,41 @@ from stackable.stack import Stack
 from stackable.utils import StackablePickler
 from stackable.network import StackableSocket, StackablePacketAssembler
 
-from messages import DispatchInquiry, Dispatched
+from messages import DispatchInquiry, Dispatched, DispatchedState
 
 from pickle import dumps
 from subprocess import Popen, PIPE
-from threading import Thread
+from threading import Thread, Lock
 from sys import argv
 
 class DispatchManager(object):
 	def __init__(self):
 		self.processes = []
+		self.plock = Lock()
 
-	def monitor(self, p, d):
+	def monitor(self, p, d, cb):
 		def wait():
 			p.wait()
-			self.processes.remove(p)
-			d.output = p.stdout.read()
-			d.cb(d)
+			res = DispatchedState(retval=None, output=p.stdout.read(), exc=None)
+			with self.plock:
+				self.processes.remove(p)
+				d.resp = res
+				cb(d)
 		Thread(target=wait).start()
 
-	def dispatch(self, d):
-		p = Popen(['pypy', '-c', 'from runnable.runnable import Runnable;from pickle import loads;from sys import argv;loads(argv[1]).execute()', dumps(d.w)], stdout=PIPE)
-		self.processes.append(p)
-		self.monitor(p, d)
-
+	def dispatch(self, d, cb):
+		with self.plock:
+			r = d.payload
+			p = Popen(['pypy', '-c', 'from pickle import loads as __ploads;from sys import argv as __argv;__ploads(__argv[1]).execute()', dumps(r)], stdout=PIPE)
+			self.processes.append(p)
+			self.monitor(p, d, cb)
 
 	def killall(self):
-		x = self.processes
-		for i in x:
-			i.kill()
-			self.processes.remove(i)
+		with self.plock:
+			x = self.processes
+			for i in x:
+				i.kill()
+				self.processes.remove(i)
 
 mgr = DispatchManager()
 
@@ -44,14 +49,11 @@ if __name__ == '__main__':
 	              StackablePickler()))
 
 	def complete_cb(d):
-		del d.cb
 		print('[DISPATCHER] Job complete')
 		stack.write(d)
 
 	while True:
 		o = stack.read()
 		if isinstance(o, DispatchInquiry):
-			if o.msg == 'dispatch':
-				print('[DISPATCHER] Dispatching job')
-				mgr.dispatch(Dispatched(o.payload, complete_cb))
+			mgr.dispatch(o, complete_cb)
 
