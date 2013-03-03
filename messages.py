1fa7fed7d63fc7819ae5378f9a9addf7439e9b92
Kenny Levinsen <kennylevinsen@gmail.com>
2013-03-03 02:37:27 +0100

    Add DispatchedState, add target, add id

--- messages.py
+++ messages.py
@@ -1,12 +1,19 @@
 class Dispatched(object):
-	def __init__(self, w=None, cb=None):
+	def __init__(self, w=None, _id=None):
 		self.w      = w
-		self.cb     = cb
-		self.retval = None
-		self.output = None
+		self.id     = _id if _id != None else id(self)
+
+class DispatchedState(object):
+	def __init__(self, retval=None, output=None, exc=None, _id=None):
+		self.retval = retval
+		self.output = output
+		self.exc    = exc
+		self.id     = _id if _id != None else id(self)
 
 class DispatchInquiry(object):
-	def __init__(self, msg=None, payload=None, resp=None):
+	def __init__(self, msg=None, payload=None, tgt=None, resp=None, _id=None):
+		self.id      = _id if _id != None else id(self)
 		self.msg     = msg
+		self.tgt     = tgt
 		self.resp    = resp
 		self.payload = payload
