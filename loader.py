0936e189272688947ffaf23cbfd37c13754f1ed6
Kenny Levinsen <kennylevinsen@gmail.com>
2013-04-27 19:12:24 +0200

    Update loader for Job id handling

--- loader.py
+++ loader.py
@@ -6,17 +6,18 @@ from sys import modules
 from types import ModuleType
 
 class DispatchLoader(object):
-	def __init__(self, ip, port):
+	def __init__(self, ip, port, job):
 		self.stack = Stack((StackableSocket(ip=ip, port=port),
 				              StackablePacketAssembler(),
 				              StackablePickler()))
+		self.job = job
 		self.cache = {}
 
 	def get_module(self, name):
 		if name in self.cache:
 			return self.cache[name]
 		else:
-			self.stack.write({'load': name})
+			self.stack.write({'load': name, 'id': self.job})
 			o = self.stack.read()
 			if o['module'] != None:
 				self.cache[name] = o['module']
