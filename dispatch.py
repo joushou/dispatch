1aa23f2b6fdfb101d23dd3be110fcaf23b8ef8c5
Kenny Levinsen <kennylevinsen@gmail.com>
2013-04-27 19:11:47 +0200

    Update Job status reporting, add Job id

--- dispatch.py
+++ dispatch.py
@@ -10,20 +10,18 @@ from threading import Thread, Lock
 from sys import argv
 
 class Job(object):
-	def __init__(self, name, p):
+	def __init__(self, name, p, _id):
 		self.name = name
 		self.p = p
+		self.id = _id
+		self.modules = []
+		self.err = ''
+		self.out = ''
 		self.alive = True
 
 	def kill(self):
 		self.p.kill()
 		self.alive = False
-
-	def stdout(self):
-		return self.p.stdout.read()
-
-	def stderr(self):
-		return self.p.stderr.read()
 
 	def monitor(self, cb):
 		def wait():
@@ -31,6 +29,11 @@ class Job(object):
 			self.alive = False
 			cb(self)
 		Thread(target=wait).start()
+
+	def status(self):
+		self.out += self.p.stdout.read()
+		self.err += self.p.stderr.read()
+		return (self.out, self.err, self.modules, self.alive)
 
 	def wait(self):
 		x = self.p.wait()
@@ -44,6 +47,7 @@ class DispatchManager(object):
 		self.cache = {}
 		self.mod_cbs = {}
 		self.stack = None
+		self.job_cnt = 0
 		if ip != None and port != None:
 			self.connect(ip, port)
 
@@ -53,27 +57,33 @@ class DispatchManager(object):
 				         	  StackablePickler()))
 
 	def monitor(self):
-		def complete_cb(d):
-			print('[DISPATCHER] --- STDOUT ---')
-			print(d.stdout())
-			print('[DISPATCHER] --- STDERR ---')
-			print(d.stderr())
-			print('[DISPATCHER] Completed', d.name)
-
 		while True:
 			o = self.stack.read()
 			if 'cmd' in o:
 				if o['cmd'] == 'execute':
-					self.dispatch(o['mod_name'], complete_cb)
+					j = self.dispatch(o['mod_name'], lambda d: self.stack.write({'status': d.status(), 'id': d.id}))
+					self.stack.write({'dispatched': j.id, 'mod_name': j.name, 'pid': j.p.pid})
 				elif o['cmd'] == 'kill':
-					self.kill(o['mod_name'])
+					job = self.get_job(o['id'])
+					job.kill()
+					self.stack.write({'status': job.status(), 'id': job.id})
+					self.processes.remove(job)
 				elif o['cmd'] == 'module':
 					name, module = o['args']['name'], o['args']['module']
 					self.cache[name] = module
 					if name in self.mod_cbs:
 						for i in self.mod_cbs[name]:
 							i(module)
+			elif 'req' in o:
+				if o['req'] == 'status':
+					job = self.get_job(o['status'])
+					self.stack.write({'status': job.status(), 'id': job.id})
 
+
+	def get_job(self, _id):
+		for i in self.processes:
+			if i.id == _id:
+				return i
 
 	def module(self, m, cb):
 		if m in self.cache:
@@ -85,36 +95,21 @@ class DispatchManager(object):
 		self.mod_cbs[m].append(cb)
 		self.stack.write({"req": "module", "args": m})
 
-	def resume(self):
-		pass
-
-	def kill(self, m):
-		for i in self.processes:
-			if i.name == m:
-				i.kill()
-				self.processes.remove(i)
-
 	def dispatch(self, name, cb):
 		with self.plock:
 			print('[DISPATCHER] Spawning', name)
 			p = Job(name, Popen(['pypy', '-c', '''
-from __future__ import print_function,absolute_import,unicode_literals,division;
-import sys as __sys;
-from loader import DispatchLoader as __l;
-__sys.meta_path = [__l("%s", %s)];
-__m__=__sys.meta_path[0].get_module(__sys.argv[1]);
-del __sys, __l;
-exec __m__''' % ("localhost", 2001), name], stdout=PIPE, stderr=PIPE))
+from sys import meta_path
+from loader import DispatchLoader as __l
+__dispatch__ = __l('%s', %s, %d)
+meta_path = [__dispatch__]
+del meta_path
+exec __dispatch__.get_module('%s')''' % ("localhost", 2001, self.job_cnt, name)], stdout=PIPE, stderr=PIPE), self.job_cnt)
+			self.job_cnt += 1
 
 			self.processes.append(p)
 			p.monitor(cb)
-
-	def killall(self):
-		with self.plock:
-			x = self.processes
-			for i in x:
-				i.kill()
-				self.processes.remove(i)
+			return p
 
 mgr = DispatchManager(argv[1], int(argv[2]))
 
@@ -132,18 +127,23 @@ class LoaderConnection(RequestObject):
 		try:
 			obj = self.stack.poll()
 			if obj != None:
-				if 'load' in obj:
-					print('[LOADER]', obj['load'])
-					mgr.module(obj['load'], lambda x: self.stack.write({'module': x}))
+				try:
+					print('[LOADER]', obj['load'], 'to Job', obj['id'])
+					def respond(x):
+						if x != None:
+							mgr.get_job(obj['id']).modules.append(obj['load'])
+						self.stack.write({'module': x})
+
+					mgr.module(obj['load'], respond)
+				except:
+					print('[LOADER] Received malformed request:', obj)
 			return True
 		except StackableError:
 			return False
-
-print("[DISPATCHER] Ready")
-
 
 server = RunnableServer(2001, LoaderConnection)
 server_thread = Thread(target=server.execute)
 server_thread.daemon = True
 server_thread.start()
+print("[DISPATCHER] Ready")
 mgr.monitor()
