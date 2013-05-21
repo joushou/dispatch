826e620aa9c36810f763d2ab5d339fe1bbebfbaa
Kenny Levinsen <kennylevinsen@gmail.com>
2013-05-22 00:23:22 +0200

    Protocol-cleanup

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ from uuid import uuid4
 
 root = RootDispatcher()
 
-root.put("welcome", compile('print("Welcome")', "<root dispatcher>", mode='exec'))
+root.put("welcome", "python", compile('print("Welcome")', "<root dispatcher>", mode='exec'), 0.1, "Greeting message")
 
 class Job(object):
 	def __init__(self, name, pid, _id):
@@ -27,12 +27,14 @@ class Job(object):
 class Dispatcher(RequestObject):
 	'The connection objects - represents a connected dispatching node'
 	def init(self):
+		self.com_id = 0
 		self.stack = Stack((StackableSocket(sock=self.conn),
 		                    StackablePacketAssembler(),
 		                    StackablePickler()))
 		self.uuid = str(uuid4())
 		self.jobs = []
 		root.register(self)
+		self.send('init', {'uuid': self.uuid})
 		self.dispatch('welcome')
 
 	def destroy(self):
@@ -41,12 +43,15 @@ class Dispatcher(RequestObject):
 		del self.stack
 
 	def dispatch(self, w):
-		self.stack.write({'cmd': 'execute', 'mod_name': w})
+		self.send('execute', {'name': w})
 
 	def find_job(self, _id):
 		for i in self.jobs:
 			if i.id == _id:
 				return i
+
+	def report_jobs(self, cb):
+		pass
 
 	def print_status(self, job):
 		print('[JOB %s-%d] ---- STDOUT ----' % (self.uuid, job.id))
@@ -57,38 +62,68 @@ class Dispatcher(RequestObject):
 		print(', '.join(job.modules))
 		print('[JOB %s-%d] Completed' % (self.uuid, job.id), job.name)
 
+	def reply(self, cmd, args):
+		self.stack.write({'cmd': cmd, 'args': args, 'com_id': self.com_id})
+
+	def send(self, cmd, args):
+		self.com_id += 1
+		self.stack.write({'cmd': cmd, 'args': args, 'com_id': self.com_id})
+
+	def handle(self, obj):
+		cmd = obj['cmd']
+		args = obj['args']
+		if cmd == 'get_module':
+			self.reply('return', {'status': 0, 'cmd': 'get_module'})
+			mod_name = args['name']
+			(version, _type, description) = root.check(mod_name)
+			mod = root.retrieve(mod_name)
+			self.send('module_update', {'name': mod_name, 'module': mod, 'version': version, 'type': _type, 'description': description})
+		elif cmd == 'probe_module':
+			self.reply('return', {'status': 0, 'cmd': 'probe_module'})
+			mod_name = args['name']
+			(version, _type, description) = root.check(mod_name)
+			self.send('module_probe', {'name': mod_name, 'version': version, 'type': _type, 'description': description})
+		elif cmd == 'probe_jobs':
+			self.reply('return', {'status': 0})
+			pass
+		elif cmd == 'push_module':
+			mod_name = args['name']
+			mod = args['module']
+			version = args['version']
+			description = args['description']
+			root.put(mod_name, mod, version, description)
+			self.reply('return', {'status': 0, 'cmd': 'push_module'})
+		elif cmd == 'dispatch':
+			mod_name = args['name']
+			targets = args['targets']
+			for target in targets:
+				client = root.get(target)
+				client.dispatch(mod_name)
+				self.reply('return', {'status': 0, 'cmd': 'dispatch', 'target': target})
+
+		# Replies from dispatch
+		elif cmd == 'dispatched':
+			mod_name = args['name']
+			job_id = args['job_id']
+			pid = args['pid']
+			self.jobs.append(Job(mod_name, pid, job_id))
+		elif cmd == 'status_update':
+			job = self.find_job(args['job_id'])
+			status = args['status']
+			job.out = status[0]
+			job.err = status[1]
+			job.modules = status[2]
+			job.alive = status[3]
+			if not job.alive:
+				self.print_status(job)
+		else:
+			print('[DISPATCHER] Unknown blob:', obj)
+
 	def receive(self):
 		try:
 			obj = self.stack.poll()
 			if obj != None:
-				if 'req' in obj:
-					if obj['req'] == 'module':
-						self.stack.write({'cmd': 'module', 'args': {'name': obj['args'], 'module': root.retrieve(obj['args'])}})
-					elif obj['req'] == 'probe_module':
-						self.stack.write({'cmd': 'module_probe', 'args': {'name': obj['args'], 'module': root.check(obj['args'])}})
-					elif obj['req'] == 'status':
-						client = root.get(obj['args']['dispatcher'])
-						job = client.find_job(obj['args']['job'])
-						self.stack.write({'status': [job.out, job.err, job.modules], 'id': job.id})
-				elif 'cmd' in obj:
-					if obj['cmd'] == 'module':
-						root.put(obj['args']['name'], obj['args']['module'])
-					elif obj['cmd'] == 'dispatch':
-						client = root.get(obj['args']['dispatcher'])
-						client.dispatch(obj['args']['module'])
-				elif 'dispatched' in obj:
-					self.jobs.append(Job(obj['mod_name'], obj['pid'], obj['dispatched']))
-				elif 'status' in obj:
-					job = self.find_job(obj['id'])
-					status = obj['status']
-					job.out = status[0]
-					job.err = status[1]
-					job.modules = status[2]
-					job.alive = status[3]
-					if not job.alive:
-						self.print_status(job)
-				else:
-					print('[DISPATCHER] Unknown blob:', obj)
+				self.handle(obj)
 			return True
 		except StackableError:
 			return False
